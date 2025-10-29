43561a59d2ea4ae48de7b3eaecd945a02c18364d
Kaique <kaiqueboscoaraujo00@gmail.com>
2025-10-29 14:13:41 -0300

    .

---
+++ etapa_3/func_dobro.py
@@ -0,0 +1,15 @@
+# Crie uma função dobro(n) que retorne o dobro de um número.
+import os
+os.system('clear')
+
+def dobro(n):
+    num_dobro = n * 2
+    return num_dobro
+
+n = 10
+n_dobro = dobro(n)
+
+print(f'{n=}')
+print(f'{n_dobro=}')
+
+
