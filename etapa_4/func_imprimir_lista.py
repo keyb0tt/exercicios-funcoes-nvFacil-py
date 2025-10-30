0c61460519d480b54f6073bfd28f34d0d825099e
Kaique <kaiqueboscoaraujo00@gmail.com>
2025-10-30 09:08:10 -0300

    .

---
+++ etapa_4/func_imprimir_lista.py
@@ -0,0 +1,12 @@
+# Crie uma função imprimir_lista(lista, titulo="Minha Lista") que mostre um título e depois 
+# todos os elementos da lista.
+import os
+os.system('clear')
+
+def imprimir_lista(lista, titulo="Minha Lista"):
+    print(f'--------------------{titulo}--------------------\n')
+    print(f'{lista}\n')
+    print(f'---------------------------------------------------\n')
+
+lista = ['Arroz', 'Fejao', 'Batata', 'Betoneira']
+imprimir_lista(lista)
