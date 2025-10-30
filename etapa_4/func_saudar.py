b47fedaa11719c0480bb9ec98b28f83266e46a30
Kaique <kaiqueboscoaraujo00@gmail.com>
2025-10-30 08:56:55 -0300

    .

---
+++ etapa_4/func_saudar.py
@@ -0,0 +1,7 @@
+# Crie uma função saudar(nome="Visitante") que mostre uma mensagem de boas-vindas,
+# usando o nome fornecido (ou “Visitante” por padrão).
+import os
+os.system('clear')
+
+def saudar(nome="Visitante"):
+    
